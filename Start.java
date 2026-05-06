55f315edb8a003836859a60c274d8dcf62bdcfb4
sharlyn77 <sharlyn.msd@gmail.com>
2026-05-06 20:53:32 +0600

    Add files via upload

---
+++ Start.java
@@ -0,0 +1,12 @@
+import Frame.*;
+
+public class Start
+{
+    public static void main(String[] args)
+    {
+	    //if want to run LibraryFrame then without comment run this below line code
+		LibraryFrame obj1 = new LibraryFrame();
+		obj1.setVisible(true);
+		
+    }
+}
